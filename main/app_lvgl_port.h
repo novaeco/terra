c7cfc34d3b1409c79020c5edeb37aac57611f7aa
novaeco <novatribolnovea@gmail.com>
2026-01-05 14:50:31 +0100

    Delete main directory

--- main/app_lvgl_port.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "esp_err.h"
-#include "esp_lcd_panel_ops.h"
-#include "lvgl.h"
-
-esp_err_t app_lvgl_port_init(esp_lcd_panel_handle_t panel, lv_display_t **out_disp);
-void app_lvgl_port_task_start(void);
--- main/display_jd9165.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "esp_err.h"
-#include "esp_lcd_panel_vendor.h"
-
-esp_err_t display_jd9165_init(esp_lcd_panel_handle_t *out_panel);

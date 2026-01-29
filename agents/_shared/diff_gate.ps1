b86b4c0b2057d05ee9e24a620f667669b3508cce
novaeco <novatribolnovea@gmail.com>
2026-01-29 11:22:03 +0100

    Delete agents directory

--- agents/_shared/diff_gate.ps1
+++
@@ -1,7 +0,0 @@
-# Run Diff Gate on a patch file
-param(
-  [Parameter(Mandatory=$true)][string]$PatchFile
-)
-
-python .\agents\_shared\diff_gate.py $PatchFile
-exit $LASTEXITCODE
--- agents/_shared/diff_gate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# Run Diff Gate on a patch file
-python3 agents/_shared/diff_gate.py "$1"
